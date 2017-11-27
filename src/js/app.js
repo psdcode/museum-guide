09d1524de9082df3f77520ba69c7f395b92e7a13
Effluxation <patrick.dorosz@gmail.com>
2017-11-27 01:16:20 -0500

    Graceful handling of fail connection to Yelp & cors-anywhere

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,8 @@ class MapViewModel {
         self.markersObservable([]);
         for (const marker of markers) {
           // Remove each marker
-          if (marker.title.toUpperCase().indexOf(searchInput.toUpperCase()) >= 0) {
+          const markerTitle = marker.title.toUpperCase();
+          if (markerTitle.indexOf(searchInput.toUpperCase()) >= 0) {
             self.markersObservable.push(marker);
             // Check if marker not already displayed to prevent blinking due to setting map again
             if (!marker.getMap()) marker.setMap(museumMap);
@@ -59,7 +60,7 @@ class MapViewModel {
         self.sort(self.markersObservable);
 
         // TODO: check after responsive
-        //museumMap.fitBounds(mapBounds);
+        // museumMap.fitBounds(mapBounds);
       }
     };
 
@@ -109,36 +110,46 @@ class MapViewModel {
       mainInfoWindow.open(museumMap, marker);
 
       // Begin fetching data from Yelp
-      getYelp(marker).then((yelpInfo) => {
+      getYelp(marker).then(yelpInfo => {
         // Check if result exists (is not undefined)
         if (yelpInfo) {
           markerContent = `<div class="title"><strong>${marker.title}</strong></div>`;
           markerContent += `<img class="yelp-img" src=${yelpInfo.image_url} alt=${marker.title}>`;
           markerContent += `<div class="yelp-container">${getRatingImg(yelpInfo.rating)}`;
           markerContent += `<a target="_blank" href="${yelpInfo.url}"><img class="yelp-logo" \
-  src="img/yelp_trademark_rgb_outline.png" srcset="img/yelp_trademark_rgb_outline_2x.png 2x" alt="Yelp Logo"></a>`;
-          markerContent += `<a class="yelp-reviews" href="${yelpInfo.url}" target="_blank">Based on <strong>\
-  ${yelpInfo.review_count}</strong> review${yelpInfo.review_count > 1 ? 's' : ''}</a>`;
-          markerContent += `<p><address>${getYelpAddressHtml(yelpInfo.location.display_address)}</address></p>`;
-          markerContent += `<p class="yelp-info">Currently <strong>${yelpInfo.is_closed ? 'CLOSED' : 'OPEN'}</strong><br>`;
+src="img/yelp_trademark_rgb_outline.png" srcset="img/yelp_trademark_rgb_outline_2x.png 2x" \
+alt="Yelp Logo"></a>`;
+          markerContent += `<a class="yelp-reviews" href="${yelpInfo.url}" target="_blank">Based \
+on <strong>${yelpInfo.review_count}</strong> review${yelpInfo.review_count > 1 ? 's' : ''}</a>`;
+          markerContent += `<p><address>${getYelpAddressHtml(yelpInfo.location.display_address)}\
+</address></p>`;
+          markerContent += `<p class="yelp-info">Currently \
+<strong>${yelpInfo.is_closed ? 'CLOSED' : 'OPEN'}</strong><br>`;
           markerContent += `Phone: ${yelpInfo.display_phone}</p></div>`;
           mainInfoWindow.setContent(markerContent);
         } else {
-        // Result is undefined, search term not in Yelp db
+        // Result is undefined, search term not in Yelp database
           markerContent = `<div class="title"><strong>${marker.title}</strong></div>`;
-          markerContent += `<p>This museum's information is not found in Yelp.com's business directory. Try a different location.</p>`;
+          markerContent += `<p>This museum's information is not found in Yelp's business \
+directory. Try a different museum location.</p>`;
           mainInfoWindow.setContent(markerContent);
         }
       })
       .catch((err) => {
-        console.log('Error during Yelp information formatting: ', err);
+        markerContent = `<div class="title"><strong>${marker.title}</strong></div>`;
+        markerContent += `<p>Unable to retrieve this museum's Yelp data due to a \
+connection error. Please try again later.</p>`;
+        mainInfoWindow.setContent(markerContent);
+        console.log(err);
       });
     }
 
     // Helper method for formatting Yelp address html string
     function getYelpAddressHtml (yelpAddress) {
       // Remove 'Japan' from address since it's redundant in the context of a map of Tokyo
-      if (yelpAddress[yelpAddress.length - 1] === 'Japan') yelpAddress = yelpAddress.slice(0, yelpAddress.length - 1);
+      if (yelpAddress[yelpAddress.length - 1] === 'Japan') {
+        yelpAddress = yelpAddress.slice(0, yelpAddress.length - 1);
+      }
       return yelpAddress.join('<br>');
     }
 
@@ -146,8 +157,8 @@ class MapViewModel {
     function getRatingImg (rating) {
       const ratingWhole = Math.floor(rating);
       const ratingHalf = (rating - ratingWhole === 0.5 ? '_half' : '');
-      return `<img class="yelp-rating" src="img/yelp_stars_reg/regular_${ratingWhole}${ratingHalf}.png"\
-srcset="img/yelp_stars_reg/regular_${ratingWhole}${ratingHalf}@2x.png 2x">`;
+      return `<img class="yelp-rating" src="img/yelp_stars_reg/regular_${ratingWhole}${ratingHalf}\
+.png" srcset="img/yelp_stars_reg/regular_${ratingWhole}${ratingHalf}@2x.png 2x">`;
     }
 
     // Helper method for formatting search string from title
@@ -168,15 +179,15 @@ ${museumMarker.position.lat()}&longitude=${museumMarker.position.lng()}`,
             'authorization': `Bearer n9BZFWy_zC3jyQyNV9u0Tdc6IhfkwyV8b4JBg2NYD9AaQuHaUx6II\
 9ukiEQp2Z03m7Cmycz29Lu2n4Gc5LPu1wDjVVCGyignkEoZn167yyq07sbPEN7gF5GzE20YWnYx`
           }
+        }).catch(err => {
+          window.alert(`Unable to retrieve this museum's Yelp data due to a \
+connection error. Please try again later.`);
+          return Promise.reject(err);
         })
-        .then((response) => response.json())
-        .then((responseJSON) => {
-          if (responseJSON.businesses) {
-            console.log(responseJSON.businesses[0]);
-            return responseJSON.businesses[0];
-          }
-        }).catch((err) => console.log('Error1: ' + err));
+        .then(response => response.json())
+        .then(responseJSON => responseJSON.businesses[0]);
     }
+  // END of method popInfoWindow(marker)
   }
 
   resetMap () {
@@ -193,12 +204,8 @@ ko.applyBindings(currentViewModel);
 
 // Map initalization function called by maps script
 function initMap () {
-  // Create new map centering on Tokyo, Japan
+  // Create new map
   museumMap = new google.maps.Map(document.querySelector('#map'), {
-    // center: {
-    //   lat: 35.6732619,
-    //   lng: 139.5703029
-    // },
     zoom: 11
   });
   mapBounds = new google.maps.LatLngBounds();
@@ -236,37 +243,6 @@ function initMap () {
   // Notify MapViewModel that google map initialization is complete
   currentViewModel.mapReady(true);
 }
-
-// function popInfoWindow (marker) {
-//   if (mainInfoWindow.marker !== marker) {
-//     mainInfoWindow.marker = marker;
-//     mainInfoWindow.setContent(`<div>${marker.title}</div>`);
-//     mainInfoWindow.open(museumMap, marker);
-//     mainInfoWindow.addListener('closeclick', function () {
-//       mainInfoWindow.setMarker = null;
-//     });
-//   }
-// }
-
-// function toggleBounceMarker (marker) {
-//   if (marker.getAnimation()) {
-//     marker.setAnimation(null);
-//   } else {
-//     markers.forEach(otherMarker => otherMarker.setAnimation(null));
-//     marker.setAnimation(google.maps.Animation.BOUNCE);
-//     setTimeout(() => marker.setAnimation(null), 1500);
-//   }
-// }
-
-// function findMarkerForBounce (viewModel, clickedMarkerTitle) {
-//   for (const marker of viewModel.markersObservable()) {
-//     if (marker.title === clickedMarkerTitle.title) {
-//       popInfoWindow(marker, mainInfoWindow);
-//       toggleBounceMarker(marker);
-//       return;
-//     }
-//   }
-// }
 
 const museums = [{
   title: 'Tokyo Metropolitan Art Museum',
