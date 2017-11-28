4eeaf7a4c94fbddffcd532fb3a8a93960b8f6e33
Effluxation <patrick.dorosz@gmail.com>
2017-11-28 15:21:12 -0500

    refactor: move model data into separate file

---
+++ src/data/model.js
@@ -0,0 +1,78 @@
+// Initial Model
+const model = {
+  initial: {
+    lat: 35.6732619,
+    lng: 139.5703029
+  },
+  museums: [{
+    "title": "Tokyo Metropolitan Art Museum",
+    "location": {
+      "lat": 35.717181,
+      "lng": 139.772818
+    },
+    "place_id": "ChIJb_o7HiiMGGARUKu8uO7fGIQ"
+  }, {
+    "title": "Tokyo Waterworks Historical Museum",
+    "location": {
+      "lat": 35.7033346,
+      "lng": 139.7604967
+    },
+    "place_id": "ChIJrVm8DD2MGGARX70ZvbreBlQ"
+  }, {
+    "title": "Mitsubishi Ichigokan Museum",
+    "location": {
+      "lat": 35.67833169999999,
+      "lng": 139.7632194
+    },
+    "place_id": "ChIJuyY-ZfqLGGAREYR9A10IXPQ"
+  }, {
+    "title": "Tokyo Toy Museum",
+    "location": {
+      "lat": 35.68973,
+      "lng": 139.718039
+    },
+    "place_id": "ChIJHeBCIOyMGGAR7BktETlUP_w"
+  }, {
+    "title": "Tokyo National Museum",
+    "location": {
+      "lat": 35.7187026,
+      "lng": 139.7763106
+    },
+    "place_id": "ChIJycHKF4OOGGARZlm_joH52qI"
+  }, {
+    "title": "Advertising Museum Tokyo",
+    "location": {
+      "lat": 35.6649201,
+      "lng": 139.7625548
+    },
+    "place_id": "ChIJ2VtGysKLGGAR2oS8_Y_o-pk"
+  }, {
+    "title": "National Museum of Western Art",
+    "location": {
+      "lat": 35.71538690000001,
+      "lng": 139.7758138
+    },
+    "place_id": "ChIJf8xB-pyOGGARizzhlNTcI7s"
+  }, {
+    "title": "Samurai Museum",
+    "location": {
+      "lat": 35.6954844,
+      "lng": 139.7035426
+    },
+    "place_id": "ChIJ__-PU9iMGGAR6KHVhaRiqH8"
+  }, {
+    "title": "National Museum of Nature and Science",
+    "location": {
+      "lat": 35.716357,
+      "lng": 139.7763826
+    },
+    "place_id": "ChIJ8Vuh65yOGGARyj4L5IBFiIk"
+  }, {
+    "title": "National Museum of Modern Art",
+    "location": {
+      "lat": 35.6905432,
+      "lng": 139.7546932
+    },
+    "place_id": "ChIJL0kSfg2MGGARKv5KX53ZZ2Y"
+  }]
+}
--- src/js/app.js
+++ src/js/app.js
@@ -217,21 +217,14 @@ connection error. Please try again later.`);
 const tokyoMuseumViewModel = new MuseumMapViewModel();
 ko.applyBindings(tokyoMuseumViewModel);
 
-function initMap () {
-  MuseumMapViewModel.initMap();
-}
-
-function errorInitMap () {
-  MuseumMapViewModel.errorInitMap();
-}
 // Map initalization function called by maps script
 function initMap () {
   // Create new map
   museumMap = new google.maps.Map(document.querySelector('#map'), {
     // Center on Tokyo
     center: {
-      lat: 35.6732619,
-      lng: 139.5703029
+      lat: model.initial.lat,
+      lng: model.initial.lng
     },
     zoom: 12
   });
@@ -251,7 +244,7 @@ function initMap () {
   const museumIconImage = 'img/temple-2.png';
 
   // Create array of Markers from provided museum info
-  for (const museum of museums) {
+  for (const museum of model.museums) {
     const newMarker = new google.maps.Marker({
       position: museum.location,
       title: museum.title,
@@ -276,88 +269,6 @@ function initMap () {
 }
 
 // Google map initial loading error callback
-MuseumMapViewModel.errorLoadMap = function () {
+function errorLoadMap () {
   alert('Unable to load Google Map at this time. Check your connection or try again later');
 }
-
-// Initial Model
-const museums = [{
-  title: 'Tokyo Metropolitan Art Museum',
-  location: {
-    lat: 35.717181,
-    lng: 139.772818
-  },
-  place_id: 'ChIJb_o7HiiMGGARUKu8uO7fGIQ'
-}, {
-  title: 'Tokyo Waterworks Historical Museum',
-  location: {
-    lat: 35.7033346,
-    lng: 139.7604967
-  },
-  place_id: 'ChIJrVm8DD2MGGARX70ZvbreBlQ'
-}, {
-  title: 'Mitsubishi Ichigokan Museum',
-  location: {
-    lat: 35.67833169999999,
-    lng: 139.7632194
-  },
-  place_id: 'ChIJuyY-ZfqLGGAREYR9A10IXPQ'
-}, {
-  title: 'Tokyo Toy Museum',
-  location: {
-    lat: 35.68973,
-    lng: 139.718039
-  },
-  place_id: 'ChIJHeBCIOyMGGAR7BktETlUP_w'
-}, {
-  title: 'Tokyo National Museum',
-  location: {
-    lat: 35.7187026,
-    lng: 139.7763106
-  },
-  place_id: 'ChIJycHKF4OOGGARZlm_joH52qI'
-}, {
-  title: 'Advertising Museum Tokyo',
-  location: {
-    lat: 35.6649201,
-    lng: 139.7625548
-  },
-  place_id: 'ChIJ2VtGysKLGGAR2oS8_Y_o-pk'
-}, {
-  title: 'National Museum of Western Art',
-  location: {
-    lat: 35.71538690000001,
-    lng: 139.7758138
-  },
-  place_id: 'ChIJf8xB-pyOGGARizzhlNTcI7s'
-}, {
-  title: 'Samurai Museum',
-  location: {
-    lat: 35.6954844,
-    lng: 139.7035426
-  },
-  place_id: 'ChIJ__-PU9iMGGAR6KHVhaRiqH8'
-}, {
-  title: 'National Museum of Nature and Science',
-  location: {
-    lat: 35.716357,
-    lng: 139.7763826
-  },
-  place_id: 'ChIJ8Vuh65yOGGARyj4L5IBFiIk'
-}, {
-  title: 'National Museum of Modern Art',
-  location: {
-    lat: 35.6905432,
-    lng: 139.7546932
-  },
-  place_id: 'ChIJL0kSfg2MGGARKv5KX53ZZ2Y'
-}
-// , {
-//   // TODO incorrect test entry
-//   title: 'jfksldjf sdlkfjsldkjf',
-//   location: {
-//     lat: 40.761484,
-//     lng: -73.977664
-//   },
-//   place_id: 'ChIJL0kSfg2MGGARKv5KX53ZZ2Y'}
-];
