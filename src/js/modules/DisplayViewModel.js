45765ae36b877c6964d9dfc14ff85f810839a991
Effluxation <patrick.dorosz@gmail.com>
2018-03-12 16:30:05 -0400

    Remove methods from constructor into class 'prototype'

--- src/js/modules/DisplayViewModel.js
+++ src/js/modules/DisplayViewModel.js
@@ -29,85 +29,24 @@ class DisplayViewModel {
       }
     }, self);
 
-    /* Instance Methods */
-    // Method to open InfoWindow using prev/next buttons
-    self.clickArrow = function (direction) {
-      // Check if current search result has more than 1 item
-      if (self.markersObservable().length > 1) {
-        const currentMarkerIndex = self.markersObservable.indexOf(GoogleMapView.mainInfoWindow.marker);
-        // Get index of target list-view entry the markers observable array
-        let neighborMarkerIndex = (currentMarkerIndex + direction) % self.markersObservable().length;
-        // Check if need to loop back to beginning of list
-        if (neighborMarkerIndex === -1) {
-          neighborMarkerIndex = self.markersObservable().length - 1;
-        }
-        const neighborMarker = self.markersObservable()[neighborMarkerIndex];
-        GoogleMapView.popInfoWindow(neighborMarker);
-      }
-    };
-
-    // Filter obsrvable location list and markers based on query
-    self.filterMarkerList = function (searchInput) {
-      // Search query is a non-empty string
-      if (searchInput !== '') {
-        // Empty the observable list
-        self.markersObservable([]);
-        GoogleMapView.markers.forEach(function (checkMarker) {
-          // Re-add marker to observable array only if marker title match search query
-          const markerTitle = checkMarker.title.toUpperCase();
-          if (markerTitle.indexOf(searchInput.toUpperCase()) >= 0) {
-            // Positive match between query and marker title
-            self.markersObservable.push(checkMarker);
-            // Check if marker not already displayed to prevent blinking due to setting map again
-            if (!checkMarker.getMap()) {
-              GoogleMapView.setMarkerMap(checkMarker, true);
-            }
-            GoogleMapView.queryBoundsExtend(checkMarker.position);
-          // Marker title did not match search query, remove it from map
-          } else {
-            GoogleMapView.setMarkerMap(checkMarker, false);
-          }
-        });
-
-        const markersLength = self.markersObservable().length;
-        // Open info window if 1 marker matches search
-        if (markersLength === 1) {
-          if (!GoogleMapView.mainInfoWindow.marker) {
-            GoogleMapView.popInfoWindow(self.markersObservable()[0]);
-          }
-          // Will set queryBounds to undefined, no bounds fit
-          GoogleMapView.queryBoundsFit(false);
-
-        // Else sort remaining markers after query and apply new bounds
-        // only if more than 1 marker matches search
-        } else if (markersLength > 1) {
-          // Close InfoWindow if open on a marker
-          GoogleMapView.closeInfoWindow();
-          self.sort(self.markersObservable);
-          // Fit query bounds
-          GoogleMapView.queryBoundsFit(true);
-        }
-
-      // Search query is empty string ''
-      } else {
-        // Display all markers on map
-        GoogleMapView.markers.forEach(function (checkMarker) {
-          if (!checkMarker.getMap()) {
-            GoogleMapView.setMarkerMap(checkMarker, true);
-          }
-        });
-        // Display all list items
-        self.markersObservable(GoogleMapView.markers);
-        self.sort(self.markersObservable);
-        self.resetMap();
-      }
-    };
-    self.getVisibleMarkers = function () {
-      return self.markersObservable();
-    };
-
     // Observable subscription for instant filtering of query results
-    self.query.subscribe(self.filterMarkerList);
+    self.query.subscribe(self.filterMarkerList.bind(self));
+  }
+
+  // Method to open InfoWindow using prev/next buttons
+  clickArrow (direction) {
+    // Check if current search result has more than 1 item
+    if (this.markersObservable().length > 1) {
+      const currentMarkerIndex = this.markersObservable.indexOf(GoogleMapView.mainInfoWindow.marker);
+      // Get index of target list-view entry in the markers observable array
+      let neighborMarkerIndex = (currentMarkerIndex + direction) % this.markersObservable().length;
+      // Check if need to loop back to beginning of list
+      if (neighborMarkerIndex === -1) {
+        neighborMarkerIndex = this.markersObservable().length - 1;
+      }
+      const neighborMarker = this.markersObservable()[neighborMarkerIndex];
+      GoogleMapView.popInfoWindow(neighborMarker);
+    }
   }
 
   // When click a location in sidebar
@@ -127,6 +66,67 @@ class DisplayViewModel {
   // Navigate to previous available marker InfoWindow in list
   clickPrevArrow () {
     DisplayViewModel.instance.clickArrow(-1);
+  }
+
+  // Filter obsrvable location list and markers based on query
+  filterMarkerList (searchInput) {
+    // Search query is a non-empty string
+    if (searchInput !== '') {
+      // Empty the observable list
+      this.markersObservable([]);
+      GoogleMapView.markers.forEach(function (checkMarker) {
+        // Re-add marker to observable array only if marker title match search query
+        const markerTitle = checkMarker.title.toUpperCase();
+        if (markerTitle.indexOf(searchInput.toUpperCase()) >= 0) {
+          // Positive match between query and marker title
+          this.markersObservable.push(checkMarker);
+          // Check if marker not already displayed to prevent blinking due to setting map again
+          if (!checkMarker.getMap()) {
+            GoogleMapView.setMarkerMap(checkMarker, true);
+          }
+          GoogleMapView.queryBoundsExtend(checkMarker.position);
+        // Marker title did not match search query, remove it from map
+        } else {
+          GoogleMapView.setMarkerMap(checkMarker, false);
+        }
+      }, this);
+
+      const markersLength = this.markersObservable().length;
+      // Open info window if 1 marker matches search
+      if (markersLength === 1) {
+        if (!GoogleMapView.mainInfoWindow.marker) {
+          GoogleMapView.popInfoWindow(this.markersObservable()[0]);
+        }
+        // Will set queryBounds to undefined, no bounds fit
+        GoogleMapView.queryBoundsFit(false);
+
+      // Else sort remaining markers after query and apply new bounds
+      // only if more than 1 marker matches search
+      } else if (markersLength > 1) {
+        // Close InfoWindow if open on a marker
+        GoogleMapView.closeInfoWindow();
+        this.sort(this.markersObservable);
+        // Fit query bounds
+        GoogleMapView.queryBoundsFit(true);
+      }
+
+    // Search query is empty string ''
+    } else {
+      // Display all markers on map
+      GoogleMapView.markers.forEach(function (checkMarker) {
+        if (!checkMarker.getMap()) {
+          GoogleMapView.setMarkerMap(checkMarker, true);
+        }
+      });
+      // Display all list items
+      this.markersObservable(GoogleMapView.markers);
+      this.sort(this.markersObservable);
+      this.resetMap();
+    }
+  }
+
+  getVisibleMarkers () {
+    return this.markersObservable();
   }
 
   // Method bound to each list element determines if selection class applied
@@ -150,7 +150,7 @@ class DisplayViewModel {
     });
   }
 
-  // Called by Reset <button>
+  // Called by Reset btn
   resetMap () {
     this.query('');
     GoogleMapView.resetMap();
@@ -159,14 +159,25 @@ class DisplayViewModel {
   // Shows/hides sidebar with hamburger <button>
   toggleSidebar () {
     const listView = document.getElementsByClassName('list-sidebar')[0];
+    const hamburgerBars = document.getElementsByClassName('hamburger__bar');
     const state = listView.classList.contains('list-sidebar--show');
     // Hides sidebar if open and vice versa
     if (state) {
       listView.classList.add('list-sidebar--hide');
       listView.classList.remove('list-sidebar--show');
+
+      // Inactivate hamburger bars color
+      for (const hamburgerBar of hamburgerBars) {
+        hamburgerBar.classList.remove('hamburger__bar--active');
+      }
     } else {
       listView.classList.remove('list-sidebar--hide');
       listView.classList.add('list-sidebar--show');
+
+      // Activate hamburger bars color
+      for (const hamburgerBar of hamburgerBars) {
+        hamburgerBar.classList.add('hamburger__bar--active');
+      }
     }
   }
 }
