f6d5a5633e460c57c77a840e9bfb2889892620e1
Effluxation <patrick.dorosz@gmail.com>
2018-03-27 00:02:06 -0400

    Initial modal class commit

---
+++ src/js/modules/Modal.js
@@ -0,0 +1,72 @@
+import spinnerHtmlString from './spinner';
+
+class Modal {
+  constructor () {
+    // Elements
+    this.modalElement = document.getElementsByClassName('modal')[0];
+    this.modalElementContent = document.getElementsByClassName('modal__content')[0];
+    this.modalElementCloseBtn = document.getElementsByClassName('modal__close-btn')[0];
+    this.loadingFormElement = document.getElementsByClassName('form__load-screen')[0];
+
+    // Variables
+    this.isInitialModal = true;
+
+    // General Initialization
+    // Inject spinner html into Modal load screen
+    this.loadingFormElement.insertAdjacentHTML('beforeend', spinnerHtmlString);
+    // Delay & Animate initial modal appearance
+    setTimeout(this.launchFirstModal.bind(this), 400);
+
+    // Initialize event handlers
+    this.events();
+  }
+
+  events () {
+    // Clicking the x close modal buttons
+    // this.closeModalButton.click(this.closeModal.bind(this));
+    // Key push event listener
+    document.addEventListener('keyup', this.keyPressHandler.bind(this));
+  }
+
+  isModalHidden () {
+    const modalState = this.modalElement.classList.contains('modal--hidden');
+    return modalState;
+  }
+
+  launchFirstModal () {
+    this.modalElementContent.classList.remove('modal__content--hidden');
+  }
+
+  openModal () {
+    // Make modal close button visible if first time modal is manually opened
+    if (this.isInitialModal === true) {
+      this.isInitialModal = false;
+      this.modalElementCloseBtn.classList.remove('modal__close-btn--hidden');
+    }
+    this.modalElement.classList.remove('modal--hidden');
+  }
+
+  closeModal () {
+    // Hide entire modal
+    this.modalElement.classList.add('modal--hidden');
+    // Hide load screen within modal
+    this.loadingFormElement.classList.remove('form__load-screen--visible');
+  }
+
+  keyPressHandler (event) {
+    if (event.keyCode === 27 && this.isInitialModal === false) {
+      this.closeModal();
+    }
+  }
+
+  openFormLoadScreen () {
+    this.loadingFormElement.classList.add('form__load-screen--visible');
+  }
+
+  submitBtnSequence () {
+    this.openFormLoadScreen();
+    setTimeout(this.closeModal.bind(this), 1500);
+  }
+}
+
+export default new Modal();
