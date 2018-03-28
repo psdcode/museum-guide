eaf7090005b9d7c3108e8214e3254807cb8b6bc9
Effluxation <patrick.dorosz@gmail.com>
2018-03-28 13:15:03 -0400

    Finish Osaka curated list
    Add kids icon type.

--- src/model/model.js
+++ src/model/model.js
@@ -50,7 +50,7 @@ export const model = {
           "lat": 35.68973,
           "lng": 139.718039
         },
-        "type": "general",
+        "type": "kids",
         "place_id": "ChIJHeBCIOyMGGAR7BktETlUP_w"
       }, {
         "title": "Tokyo National Museum",
@@ -112,85 +112,85 @@ export const model = {
         "lng": 139.638056,
       },
       "locations": [{
-        "title": "Yokohama Metropolitan Art Museum",
+        "title": "Yokohama Cup Noodles Museum",
         "position": {
-          "lat": 35.717181,
-          "lng": 139.772818
-        },
-        "type": "art",
-        "place_id": "ChIJb_o7HiiMGGARUKu8uO7fGIQ"
-      }, {
-        "title": "Yokohama Waterworks Historical Museum",
-        "position": {
-          "lat": 35.7033346,
-          "lng": 139.7604967
-        },
-        "type": "history",
-        "place_id": "ChIJrVm8DD2MGGARX70ZvbreBlQ"
-      }, {
-        "title": "Mitsubishi Ichigokan Museum",
-        "position": {
-          "lat": 35.67833169999999,
-          "lng": 139.7632194
-        },
-        "type": "art",
-        "place_id": "ChIJuyY-ZfqLGGAREYR9A10IXPQ"
-      }, {
-        "title": "Yokohama Toy Museum",
-        "position": {
-          "lat": 35.68973,
-          "lng": 139.718039
+          "lat" : 35.4554746,
+          "lng" : 139.6388986
         },
         "type": "general",
-        "place_id": "ChIJHeBCIOyMGGAR7BktETlUP_w"
+        "place_id": "ChIJ3ZNhe1dcGGARvjq5QHdmaHM"
       }, {
-        "title": "Yokohama National Museum",
+        "title": "Japan Coast Guard Museum Yokohama",
         "position": {
-          "lat": 35.7187026,
-          "lng": 139.7763106
+          "lat" : 35.4543846,
+          "lng" : 139.644207
         },
-        "type": "history",
-        "place_id": "ChIJycHKF4OOGGARZlm_joH52qI"
+        "type": "general",
+        "place_id": "ChIJH19OaFZcGGARAc-aTS83zVc"
       }, {
-        "title": "Advertising Museum Yokohama",
+        "title": "",
         "position": {
-          "lat": 35.6649201,
-          "lng": 139.7625548
+
+
         },
-        "type": "art",
-        "place_id": "ChIJ2VtGysKLGGAR2oS8_Y_o-pk"
+        "type": "",
+        "place_id": ""
       }, {
-        "title": "National Museum of Western Art",
+        "title": "",
         "position": {
-          "lat": 35.71538690000001,
-          "lng": 139.7758138
+
+
         },
-        "type": "art",
-        "place_id": "ChIJf8xB-pyOGGARizzhlNTcI7s"
+        "type": "",
+        "place_id": ""
       }, {
-        "title": "Samurai Museum",
+        "title": "",
         "position": {
-          "lat": 35.6954844,
-          "lng": 139.7035426
+
+
         },
-        "type": "history",
-        "place_id": "ChIJ__-PU9iMGGAR6KHVhaRiqH8"
+        "type": "",
+        "place_id": ""
       }, {
-        "title": "National Museum of Nature and Science",
+        "title": "",
         "position": {
-          "lat": 35.716357,
-          "lng": 139.7763826
+
+
         },
-        "type": "science",
-        "place_id": "ChIJ8Vuh65yOGGARyj4L5IBFiIk"
+        "type": "",
+        "place_id": ""
       }, {
-        "title": "National Museum of Modern Art",
+        "title": "",
         "position": {
-          "lat": 35.6905432,
-          "lng": 139.7546932
+
+
         },
-        "type": "art",
-        "place_id": "ChIJL0kSfg2MGGARKv5KX53ZZ2Y"
+        "type": "",
+        "place_id": ""
+      }, {
+        "title": "",
+        "position": {
+
+
+        },
+        "type": "",
+        "place_id": ""
+      }, {
+        "title": "",
+        "position": {
+
+
+        },
+        "type": "",
+        "place_id": ""
+      }, {
+        "title": "",
+        "position": {
+
+
+        },
+        "type": "",
+        "place_id": ""
       }]
     },
     {
@@ -203,85 +203,85 @@ export const model = {
         "lng": 135.502222
       },
       "locations": [{
-        "title": "Osaka Metropolitan Art Museum",
+        "title": "Osaka Museum of History",
         "position": {
-          "lat": 35.717181,
-          "lng": 139.772818
-        },
-        "type": "art",
-        "place_id": "ChIJb_o7HiiMGGARUKu8uO7fGIQ"
-      }, {
-        "title": "Osaka Waterworks Historical Museum",
-        "position": {
-          "lat": 35.7033346,
-          "lng": 139.7604967
+          "lat": 34.682652,
+          "lng": 135.520800
         },
         "type": "history",
-        "place_id": "ChIJrVm8DD2MGGARX70ZvbreBlQ"
+        "place_id": "ChIJR5zaJTLnAGARyuJ_yX2DhOk"
       }, {
-        "title": "Mitsubishi Ichigokan Museum",
+        "title": "Osaka Science Museum",
         "position": {
-          "lat": 35.67833169999999,
-          "lng": 139.7632194
-        },
-        "type": "art",
-        "place_id": "ChIJuyY-ZfqLGGAREYR9A10IXPQ"
-      }, {
-        "title": "Osaka Toy Museum",
-        "position": {
-          "lat": 35.68973,
-          "lng": 139.718039
-        },
-        "type": "general",
-        "place_id": "ChIJHeBCIOyMGGAR7BktETlUP_w"
-      }, {
-        "title": "Osaka National Museum",
-        "position": {
-          "lat": 35.7187026,
-          "lng": 139.7763106
-        },
-        "type": "history",
-        "place_id": "ChIJycHKF4OOGGARZlm_joH52qI"
-      }, {
-        "title": "Advertising Museum Osaka",
-        "position": {
-          "lat": 35.6649201,
-          "lng": 139.7625548
-        },
-        "type": "art",
-        "place_id": "ChIJ2VtGysKLGGAR2oS8_Y_o-pk"
-      }, {
-        "title": "National Museum of Western Art",
-        "position": {
-          "lat": 35.71538690000001,
-          "lng": 139.7758138
-        },
-        "type": "art",
-        "place_id": "ChIJf8xB-pyOGGARizzhlNTcI7s"
-      }, {
-        "title": "Samurai Museum",
-        "position": {
-          "lat": 35.6954844,
-          "lng": 139.7035426
-        },
-        "type": "history",
-        "place_id": "ChIJ__-PU9iMGGAR6KHVhaRiqH8"
-      }, {
-        "title": "National Museum of Nature and Science",
-        "position": {
-          "lat": 35.716357,
-          "lng": 139.7763826
+          "lat": 34.691384,
+          "lng": 135.491479
         },
         "type": "science",
-        "place_id": "ChIJ8Vuh65yOGGARyj4L5IBFiIk"
+        "place_id": "ChIJrVid0PbmAGARVvoeAwYmgg4"
       }, {
-        "title": "National Museum of Modern Art",
+        "title": "The National Museum of Art, Osaka",
         "position": {
-          "lat": 35.6905432,
-          "lng": 139.7546932
+          "lat": 34.691829,
+          "lng": 135.491948
         },
         "type": "art",
-        "place_id": "ChIJL0kSfg2MGGARKv5KX53ZZ2Y"
+        "place_id": "ChIJq6o6zPbmAGAR5Q9OsQN_voo"
+      }, {
+        "title": "Museum of Oriental Ceramics, Osaka",
+        "position": {
+          "lat": 34.693484,
+          "lng":  135.505417
+        },
+        "type": "art",
+        "place_id": "ChIJhQQzld3mAGAR1oYbB9a77vQ"
+      }, {
+        "title": "Abeno Harukas Art Museum",
+        "position": {
+          "lat" : 34.6457933,
+          "lng" : 135.5140916
+        },
+        "type": "art",
+        "place_id": "ChIJw_dasfHdAGARPIYrcFpn6aY"
+      }, {
+        "title": "Osaka Museum of Housing and Living",
+        "position": {
+          "lat": 34.710397,
+          "lng": 135.511641
+        },
+        "type": "history",
+        "place_id": "ChIJ7xEdT7nmAGARYQc8ZWRPI5Q"
+      }, {
+        "title": "Osaka International Peace Center",
+        "position": {
+          "lat" : 34.6818454,
+          "lng" : 135.5298354
+        },
+        "type": "history",
+        "place_id": "ChIJ0X861cvgAGARSrukky_tpoI"
+      }, {
+        "title": "Mint Museum",
+        "position": {
+          "lat" : 34.6957662,
+          "lng" : 135.5216638
+        },
+        "type": "history",
+        "place_id": "ChIJK4WCStTmAGAREg2BPGKLD-Q"
+      }, {
+        "title": "Kids Plaza Osaka",
+        "position": {
+          "lat" : 34.7045728,
+          "lng" : 135.5104418
+        },
+        "type": "kids",
+        "place_id": "ChIJEZamxsDmAGARdScVXFQanLU"
+      }, {
+        "title": "Osaka Museum of Natural History",
+        "position": {
+          "lat" : 34.6102774,
+          "lng" : 135.5216484
+        },
+        "type": "science",
+        "place_id": "ChIJDXa8qOjmAGARGeBCBnhGKOY"
       }]
     }
   ]
