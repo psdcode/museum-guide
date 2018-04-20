8d81dc8d81af273fb3e23602a92c9651eaa904b4
Effluxation <patrick.dorosz@gmail.com>
2018-04-19 22:47:43 -0400

    Fix centering of text inside arrow buttons

--- src/js/modules/GoogleMapView.js
+++ src/js/modules/GoogleMapView.js
@@ -311,11 +311,9 @@ class GoogleMapView {
       function getInfoWindowArrowsHtml () {
         let infoWindowArrows = `<div class="info-window__arrows">`;
         infoWindowArrows += `<a href="#" role="button" class="btn info-window__arrows-prev"`;
-        // infoWindowArrows += `>&lt;</a>`;
-        infoWindowArrows += `><span class="bof">&lt;</span></a>`;
+        infoWindowArrows += `><span class="info-window__arrows-text">&lt;</span></a>`;
         infoWindowArrows += `<a href="#" class="btn info-window__arrows-next" role="button"`;
-        // infoWindowArrows += `>&gt;</a>`;
-        infoWindowArrows += `><span class="bof">&gt;</span></a>`;
+        infoWindowArrows += `><span class="info-window__arrows-text">&gt;</span></a>`;
         infoWindowArrows += `</div>`;
         return infoWindowArrows;
       }
